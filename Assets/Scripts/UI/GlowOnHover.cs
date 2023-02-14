47c125c1e9bd39e0013097ba79a39ea4614539fb
Semih Teke <semih.teke@student.howest.be>
2023-02-14 16:35:41 +0100

    Playerfeedback when selecting cards

---
+++ Assets/Scripts/UI/GlowOnHover.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GlowOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private float _basePositionY;
+    private RectTransform _picture;
+    private void Start()
+    {
+         _picture = GetComponent<RectTransform>();
+        _basePositionY = _picture.position.y;
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _picture.anchoredPosition = new Vector2(_picture.anchoredPosition.x, _basePositionY+20);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _picture.anchoredPosition = new Vector2(_picture.anchoredPosition.x, _basePositionY);
+    }
+}
