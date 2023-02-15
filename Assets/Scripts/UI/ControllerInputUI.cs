810f226f7f86641bfb93fb856299f009340c829e
Semih Teke <semih.teke@student.howest.be>
2023-02-15 14:02:53 +0100

    Got cards working again
    Changed the Map prefab to work with the cards

--- Assets/Scripts/UI/ControllerInputUI.cs
+++ Assets/Scripts/UI/ControllerInputUI.cs
@@ -2,73 +2,101 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class ControllerInputUI : MonoBehaviour
 {
     private GraphicRaycaster _graphicRaycaster;
     public Camera Cam;
+
+    private GameObject _sphere;
+    private bool _isDragging;
+
     private void Start()
     {
         _graphicRaycaster = GetComponentInParent<GraphicRaycaster>();
-       
+
+
     }
     private void Update()
     {
-       
+
         MovePointer();
 
-        
+
         var pointer = new PointerEventData(EventSystem.current) { position = transform.position };
 
         //Cast ray towards Canvas elements through pointer's position and store result
         List<RaycastResult> raycastResults = new List<RaycastResult>();
         _graphicRaycaster.Raycast(pointer, raycastResults);
 
-        //Raycast towards the land while dragging
-        RaycastHit hit;
-        Ray ray = Cam.ScreenPointToRay(pointer.position);
 
 
-        foreach (RaycastResult raycastResult in raycastResults)
+        //UI element hit by the graphics raycaster
+        UIBehaviour ui;
+        if (raycastResults.Count > 0)
+            ui = raycastResults[0].gameObject.GetComponent<UIBehaviour>();
+        else
         {
-            var ui = raycastResult.gameObject.GetComponent<UIBehaviour>();
+            ui = null;
+        }
 
-            //ExecuteEvents.Execute(raycastResult.gameObject, pointer, ExecuteEvents.pointerEnterHandler);
-            //ExecuteEvents.Execute(raycastResult.gameObject, pointer, ExecuteEvents.beginDragHandler);
-            //xecuteEvents.Execute(raycastResult.gameObject, pointer, ExecuteEvents.pointerExitHandler);
-            if (ui)
+        if (ui)
+        {
+            ExecuteEvents.Execute(ui.gameObject, pointer, ExecuteEvents.pointerEnterHandler);
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                //if (Input.GetKeyDown(KeyCode.Space))
-                //{
-                   
-                //        Debug.Log($"Hit {hit.collider.gameObject.name}");
-                //        ExecuteEvents.Execute(hit.collider.gameObject, pointer, ExecuteEvents.beginDragHandler);
-                   
-                //}
+                Debug.Log($"Hit {ui.name}");
+                ExecuteEvents.Execute(ui.gameObject, pointer, ExecuteEvents.beginDragHandler);
 
-                //if (Input.GetKey(KeyCode.Space))
-                //{
-                   
-                //        ExecuteEvents.Execute(hit.collider.gameObject, pointer, ExecuteEvents.dragHandler);
-                    
-                //}
+            }
 
-                //if (Input.GetKeyUp(KeyCode.Space))
-                //{
-                    
-                //        ExecuteEvents.Execute(hit.collider.gameObject, pointer, ExecuteEvents.endDragHandler);
-                    
-                //}
+            if (Input.GetKey(KeyCode.Space))
+            {
+                ExecuteEvents.Execute(ui.gameObject, pointer, ExecuteEvents.dragHandler);
+                _isDragging = true;
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                ExecuteEvents.Execute(ui.gameObject, pointer, ExecuteEvents.endDragHandler);
+
             }
         }
+
+
+        RaycastHit hit;
+        Ray ray = Cam.ScreenPointToRay(pointer.position);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+
+            //Debug.DrawLine(ray.origin, hit.point,Color.red);
+
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //Spawn primitive sphere
+                _sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                _sphere.GetComponent<SphereCollider>().enabled = false;
+                _sphere.transform.localScale = Vector3.one * 100;
+                _sphere.transform.position = new Vector3(-500, -500, -500);
+            }
+
+            Debug.Log(_isDragging);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                _sphere.transform.position = hit.point;
+
+            }
+        }
+
+
     }
 
     void MovePointer()
     {
         //Change input later to use joystick axes
-         if (Input.GetKey(KeyCode.Z))
+        if (Input.GetKey(KeyCode.Z))
         {
             transform.position += Vector3.up;
         }
--- Assets/Scripts/UI/OnHoverEffect.cs
+++ Assets/Scripts/UI/OnHoverEffect.cs
@@ -9,17 +9,20 @@ public class OnHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     private RectTransform _picture;
     private void Start()
     {
-         _picture = GetComponent<RectTransform>();
-        _basePositionY = _picture.position.y;
+         _picture = gameObject.GetComponent<RectTransform>();
+        _basePositionY = _picture.anchoredPosition.y;
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
-        
+       
+        Debug.Log("PointerEnter");
         _picture.anchoredPosition = new Vector2(_picture.anchoredPosition.x, _basePositionY+20);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        Debug.Log("PointerExit");
+
         _picture.anchoredPosition = new Vector2(_picture.anchoredPosition.x, _basePositionY);
     }
 }
